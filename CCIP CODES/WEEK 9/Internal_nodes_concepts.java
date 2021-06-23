2e78e48c425d6d14f24a85f2c945195d60f1a460
YASH KUMAR <yaku15480@gmail.com>
2021-06-23 12:29:39 +0530

    main

--- CCIP CODES/WEEK 9/Internal_nodes_concepts.java
+++ CCIP CODES/WEEK 9/Internal_nodes_concepts.java
@@ -4,15 +4,14 @@ import java.lang.*;
 
 public class Solution {
 
-    public static void main(String[] args) 
-    {
-      Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int x=sc.nextInt();
-        
-        int dif=x-1;
-        System.out.println((x*n)-dif);
-        
-        
-    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int x = sc.nextInt();
+
+    int dif = x - 1;
+    System.out.println((x * n) - dif);
+
+    // yash kumar
+  }
 }
