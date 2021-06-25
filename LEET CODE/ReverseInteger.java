52d013e1d24569296a9218b99a51b57398fa2d2e
YASH KUMAR <yaku15480@gmail.com>
2021-06-25 23:06:31 +0530

    main

--- LEET CODE/ReverseInteger.java
+++ LEET CODE/ReverseInteger.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+class Solution1 {
     public int reverse(int x) {
         int rev = 0;
         Math.abs(x);
@@ -15,17 +15,16 @@ class Solution {
     }
 }
 
-public class ReverseInteger {
+public class yash1 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.close();
-        Solution yash = new Solution();
+        Solution1 yash = new Solution1();
 
-        if (n == 0)
+        if (n == 0 || n >= 536871066)
             System.out.print("0");
-        if (n > 0)
+        if (n > 0 && n < 536871066)
             System.out.println(yash.reverse(n));
         if (n < 0)
             System.out.println("-" + (yash.reverse(n)));
