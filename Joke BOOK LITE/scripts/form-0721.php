8d77df3ff69ee45b5c0407abf4e03f291e80ba4b
YASH KUMAR <yaku15480@gmail.com>
2021-07-07 11:21:35 +0530

    JBL UPDATE

---
+++ Joke BOOK LITE/scripts/form-0721.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'xboom15480@gmail.com',
+    'to' => 'yaku15480@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
