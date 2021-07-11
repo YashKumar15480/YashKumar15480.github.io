f790dd0ae95080a381492946ed8e22b2b6d04069
YASH KUMAR <yaku15480@gmail.com>
2021-07-11 21:56:42 +0530

    LEET CODE

---
+++ LEET CODE/Remove_Element.java
@@ -0,0 +1,28 @@
+class Solution1221 {
+    public int removeElement(int[] nums, int val) {
+        int size = nums.length;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
+/**
+ * Remove_Element
+ */
+public class Remove_Element {
+
+    public static void main(String[] args) {
+        /*
+         * On leet code we don't need to call main method it directly linked to solution
+         * class you just need to write methods in solutions class and return the value
+         * according to method's parameters
+         * 
+         */
+    }
+}
