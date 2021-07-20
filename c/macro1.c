2166599d898f6764ace4be1e7ac6dbb53592492a
YASH KUMAR <yaku15480@gmail.com>
2021-07-20 11:03:07 +0530

    Main

---
+++ c/macro1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define pi 3.14
+#define area pi*r*r
+int main()
+{
+  int r;
+  printf("Enter the radius of circle => "); scanf("%d",&r);
+  printf("Area of Cirlce is => %.3f",area);
+  return 0;
+
+}
