584907a3493fd3f7166fe86733c6e02228210799
YASH KUMAR <yaku15480@gmail.com>
2021-07-22 10:38:14 +0530

    DS

--- DATA STRUCTURE/Strassens_Matrix.c
+++ DATA STRUCTURE/Strassens_Matrix.c
@@ -4,12 +4,12 @@ int main()
    int a[2][2],b[2][2],c[2][2],i,j;
    int m1,m2,m3,m4,m5,m6,m7,m8;
 
-printf("Enter the 4 of matirx 1 = ");
+printf("Enter the 4 integer of matirx 1 = ");
   for(i=0;i<2;i++)
    for(j=0;j<2;j++)
      scanf("%d",&b[i][j]);
 
-printf("Enter the 4 of matirx 2 = ");
+printf("Enter the 4 interger of matirx 2 = ");
   for(i=0;i<2;i++)
    for(j=0;j<2;j++)
      scanf("%d",&c[i][j]);
@@ -28,12 +28,12 @@ a[0][1] = m4+m6;
 a[1][0] = m5+m7;
 a[1][1] = m1-m3-m4-m5;
 
-printf("Multiplication is => ");
+printf("Multiplication is =>\n");
 for(i=0;i<2;i++)
 {
    for(j=0;j<2;j++)
   {
-     printf("%d",a[i][j]);
+     printf(" %5d ",a[i][j]);
   }
   printf("\n");
 } 
