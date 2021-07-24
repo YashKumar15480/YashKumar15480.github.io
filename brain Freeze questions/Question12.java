338da3ba5322dc0ac22606c1f39fd3f0e94edf05
YASH KUMAR <yaku15480@gmail.com>
2021-07-24 09:28:07 +0530

    BFQs

---
+++ brain Freeze questions/Question12.java
@@ -0,0 +1,23 @@
+
+public class Question12 {
+
+    void show() {
+
+        System.out.println("JAVA");
+
+    }
+
+    void display() {
+        System.out.println("World");
+    }
+
+    Question12() {
+        System.out.println("Hellow");
+
+    }
+
+    public static void main(String[] args) {
+        Question12 a = new Question12();
+        a.show();
+    }
+}
---
+++ brain Freeze questions/Question13.java
@@ -0,0 +1,24 @@
+class A4 {
+
+    void show() {
+
+        System.out.println("JAVA");
+
+    }
+
+    void display() {
+        System.out.println("World");
+    }
+
+    A4() {
+        System.out.println("Hellow");
+
+    }
+}
+
+public class Question13 {
+    public static void main(String[] args) {
+        A4 a = new A4();
+        a.display();
+    }
+}
