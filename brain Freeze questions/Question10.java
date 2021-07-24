9bf5f30b1b8560d90660465359bb16a79cca7073
YASH KUMAR <yaku15480@gmail.com>
2021-07-24 09:07:53 +0530

    BFQs

--- brain Freeze questions/Question10.java
+++ brain Freeze questions/Question10.java
@@ -2,7 +2,7 @@
 public class Question10 {
     public static void main(String[] args) {
         var s = "KIET GROUP [][[]&^*@OF INSTITUTIONS";
-        s = s.replaceAll("(.*)P(.*)", "STUDENTS");
+        s = s.replaceAll("(.*)OF(.*)", "STUDENTS");
         System.out.println(s.replace("&", ""));
     }
 }
---
+++ brain Freeze questions/Question11.java
@@ -0,0 +1,15 @@
+public class Question11 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+// *
+// **
+// ***
+// ****
+// *****
