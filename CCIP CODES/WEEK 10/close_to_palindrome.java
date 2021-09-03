add01f720f59155d2be08eef02b7fcf04ffcd70c
YASH KUMAR <yaku15480@gmail.com>
2021-09-03 22:04:55 +0530

    CCIP

--- CCIP CODES/WEEK 10/close_to_palindrome.java
+++ CCIP CODES/WEEK 10/close_to_palindrome.java
@@ -1,44 +1,45 @@
 import java.io.*;
 import java.util.*;
-    
-public class solutions
-{
-    public static void main(String args[])
-    {
-        int i,j;
-        
+
+public class solutions {
+    public static void main(String args[]) {
+        int i, j;
+
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        
+        int n = sc.nextInt();
+
         String s = String.valueOf(n);
         StringBuffer sb = new StringBuffer(s);
         String b = sb.reverse().toString();
-        
-        if(s.equals(b)) System.out.print(n);
-        else
-        {
-            //negative loop
-            for(i=n-1;i>=0;i--)
-            {
+
+        if (s.equals(b))
+            System.out.print(n);
+        else {
+            // negative loop
+            for (i = n - 1; i >= 0; i--) {
                 String s1 = String.valueOf(i);
                 StringBuffer sb1 = new StringBuffer(s1);
                 String b1 = sb1.reverse().toString();
-                if(s1.equals(b1)) break;
+                if (s1.equals(b1))
+                    break;
             }
-            //positive loop
-            //YASH KUMAR
-            for(j=n+1;j<=9999;j++)
-            {
+            // positive loop
+            // YASH KUMAR
+            for (j = n + 1; j <= 9999; j++) {
                 String s2 = String.valueOf(j);
                 StringBuffer sb2 = new StringBuffer(s2);
                 String b2 = sb2.reverse().toString();
-                if(s2.equals(b2)) break;
+                if (s2.equals(b2))
+                    break;
             }
-           
-            int ans1 = n-i; 
-            int ans2 = j-n;
-            
-            if(ans1>ans2)System.out.print(j);
-            else System.out.print(i);
-       }  
-               
+
+            int ans1 = n - i;
+            int ans2 = j - n;
+
+            if (ans1 > ans2)
+                System.out.print(j);
+            else
+                System.out.print(i);
+        }
+    }
+}
