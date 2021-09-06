d3e00e4f17f0e453da956dddc111b93a6fb4f245
YASH KUMAR <yaku15480@gmail.com>
2021-09-06 13:46:12 +0530

    CCIP

--- CCIP CODES/WEEK 1/Sum_Positive_Odd_Index.java
+++ CCIP CODES/WEEK 1/Sum_Positive_Odd_Index.java
@@ -4,13 +4,13 @@ public class Sum_Positive_Odd_Index {
 
     public static void main(String args[]) {
         int n, i, sum = 0;
-        Scanner ss = new Scanner(System.in);
-        n = ss.nextInt();
+        Scanner yash = new Scanner(System.in);
+        n = yash.nextInt();
 
         int ar[] = new int[n];
 
         for (i = 1; i < n; i++) {
-            ar[i] = ss.nextInt();
+            ar[i] = yash.nextInt();
             if ((i % 2 != 0) && (ar[i] > 0)) {
                 sum = sum + ar[i];
             }
