a907102b223df27e8aa1a6b7d1871c02fd9fac9c
YASH KUMAR <yaku15480@gmail.com>
2021-09-10 22:58:58 +0530

    coursera

---
+++ YK_Coursera_Assignments/Module_4_Solution/SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
