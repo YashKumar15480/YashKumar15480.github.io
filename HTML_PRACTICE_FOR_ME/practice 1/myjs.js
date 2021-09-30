c8023601769e3b7dbc39f219a9bd3e5d7d27b4e0
YASH KUMAR <yaku15480@gmail.com>
2021-09-30 10:46:48 +0530

    p

---
+++ HTML_PRACTICE_FOR_ME/practice 1/myjs.js
@@ -0,0 +1,20 @@
+// function change() {
+//  let id = document.getElementById("id1")
+//  id.innerHTML = "GHAZIYABAD";    
+// }
+
+var id = document.getElementById("id1")
+id.addEventListener('click',function(){
+id.innerHTML = "Ghaziyabad"
+})
+
+// function sum(...ashu){
+//     return ashu.reduce((a,b)=>{ 
+//         return a+b
+//     })
+// }
+// console.log(sum(1,2,3,4,5));
+// console.log(sum(15,2,3,111,5,6));
+
+
+
