23650b567c80d0d51e0cfa28c94813e55d87de2c
YASH KUMAR <yaku15480@gmail.com>
2021-10-18 00:27:14 +0530

    p

---
+++ HTML_PRACTICE_FOR_ME/style_using_js/js7.js
@@ -0,0 +1,12 @@
+let a = [12,23,34,3124,32,4,213,214,31,21,3,413,"sdf","f","sdf","df","ds","f","d"]
+
+function nacco(str) {
+    document.getElementById("1").innerHTML =str
+}
+nacco(a)
+function change1(){
+    document.write("CHAPPAL CHOR")
+}
+
+    document.getElementById("1").style.color = "green"
+
