e202a497d2b7e09041b85862c9cf825ef410dcc9
YASH KUMAR <yaku15480@gmail.com>
2021-10-17 21:47:49 +0530

    p

--- HTML_PRACTICE_FOR_ME/image_change_P6/js6.js
+++ HTML_PRACTICE_FOR_ME/image_change_P6/js6.js
@@ -2,5 +2,13 @@ function change() {
     document.getElementById("1").src = "waterjug1.jpeg"
 }
 function change2(){
-    document.getElementById("2").src = "bfs1.jpg"
+    document.getElementById("2").src = "waterjug1.jpeg"
+    // document.getElementById("3").innerHTML ="yuguyg"
+}
+function change3(){
+ document.getElementById("1").src = "bfs1.jpg"
+}
+
+function change4(){
+   document.getElementById("2").src = "#"
 }
