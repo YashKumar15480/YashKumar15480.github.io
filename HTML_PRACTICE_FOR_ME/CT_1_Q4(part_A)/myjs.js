dfb86acaf3b7aa37dbf7476ff0c76f15246e4c89
YASH KUMAR <yaku15480@gmail.com>
2021-12-12 00:20:00 +0530

    P

---
+++ HTML_PRACTICE_FOR_ME/CT_1_Q4(part_A)/myjs.js
@@ -0,0 +1,21 @@
+let st = []
+st.push(1);
+st.push(2);
+st.push(3);
+st.push("yash");
+console.log(st);
+console.log(st.indexOf("yash"));
+console.log(st.length);
+console.log(st.pop());
+
+let st2 = ["MCA", "3", 'c']
+
+// console.log(st.concat(st2));
+// console.log(st[3].concat(st2));
+
+(st[2] == st2[1])? console.log("TRUE"):console.log("FALSE");
+(st[2] === st2[1])? console.log("TRUE"):console.log("False");
+
+
+
+
