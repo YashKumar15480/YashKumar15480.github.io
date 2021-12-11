0498cb8b3b54b7dfbb3ff5ba52355a81419288e1
YASH KUMAR <yaku15480@gmail.com>
2021-12-12 00:28:58 +0530

    p

---
+++ HTML_PRACTICE_FOR_ME/CT_1_Q5(part A)/myjs.js
@@ -0,0 +1,14 @@
+//synchronous programming 
+
+console.log("HI");
+console.log("Let us see what happens");
+console.log("End");
+
+console.log("///////////////");
+//asynchornous programming 
+console.log("Hi");
+    setTimeout(() => {
+        console.log("Let us see what happens");
+    }, 2000);
+   console.log("End");
+    
