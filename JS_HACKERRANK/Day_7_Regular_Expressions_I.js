49043ac20e0c74607397571549352c8f952c4d37
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-06 21:51:15 +0530

    js

---
+++ JS_HACKERRANK/Day_7_Regular_Expressions_I.js
@@ -0,0 +1,22 @@
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    var string1 = readLine();
+    var vowel = "aeiou"
+    
+    if(string1.startsWith(vowel))
+    {
+        if(string1.charAt(0) == string.charAt(string1.length-1))
+        return re;
+    }
+    
+     /*
+     * Do not remove the return statement
+     */
+    
+    
+    return re;
+}
