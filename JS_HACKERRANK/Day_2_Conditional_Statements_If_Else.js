705b7e544f3c47ab0a3902a69c7c37742f529be3
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-06 21:46:09 +0530

    js

---
+++ JS_HACKERRANK/Day_2_Conditional_Statements_If_Else.js
@@ -0,0 +1,15 @@
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    
+    if(score > 25 && score<=30)grade = 'A';
+    if(score > 20 && score<=25)grade = 'B';
+    if(score > 15 && score<=20)grade = 'C';
+    if(score > 10 && score<=15)grade = 'D';
+    if(score >  5 && score<=10)grade = 'E';
+    if(score >  0 && score<= 5)grade = 'F';
+
+
+    
+    return grade;
+}
