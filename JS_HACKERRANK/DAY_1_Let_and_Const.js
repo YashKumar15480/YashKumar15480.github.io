b6af6774b395e8e062a41b804f29ac80cd3cb292
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-06 21:44:38 +0530

    js

---
+++ JS_HACKERRANK/DAY_1_Let_and_Const.js
@@ -0,0 +1,11 @@
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+   const PI = Math.PI;
+   var radius = readLine();    
+    // Print the area of the circle:
+    console.log(PI*(radius*radius));
+    
+    // Print the perimeter of the circle:
+    console.log(2*PI*(radius));
+}
