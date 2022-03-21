cf64217e1a3be8c04c983235356140b82c11790e
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-21 22:26:24 +0530

    js

---
+++ JS_HACKERRANK/Day_3_Arrays.js
@@ -0,0 +1,13 @@
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(arr) {
+    
+
+    var max = Math.max.apply(null, arr); // get the max of the array
+    arr.splice(arr.indexOf(max), 1); // remove max from the array
+    return Math.max.apply(null, arr); // get the 2nd max
+
+}
