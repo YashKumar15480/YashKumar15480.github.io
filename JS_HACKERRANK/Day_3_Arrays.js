c98a6b0e1c5acc9f993527bdf5bec22a1d463065
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-22 23:43:55 +0530

    js

--- JS_HACKERRANK/Day_3_Arrays.js
+++ JS_HACKERRANK/Day_3_Arrays.js
@@ -9,5 +9,5 @@ function getSecondLargest(arr) {
     var max = Math.max.apply(null, arr); // get the max of the array
     arr.splice(arr.indexOf(max), 1); // remove max from the array
     return Math.max.apply(null, arr); // get the 2nd max
-
+// yash
 }
