a992f70c1823bd97fb7a8cfa66e45c2ea27963d3
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-26 23:57:35 +0530

    js

--- JS_HACKERRANK/DAY_0_Data_Types.js
+++ JS_HACKERRANK/DAY_0_Data_Types.js
@@ -31,9 +31,5 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     console.log(firstString+""+secondString);
     
     
-    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    
-    
-    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    
+   
 }
