d0a2e70828caca7b7e8f0502451bc6d47b4bf035
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-03-27 21:39:02 +0530

    js

--- JS_HACKERRANK/DAY_0_Data_Types.js
+++ JS_HACKERRANK/DAY_0_Data_Types.js
@@ -1,16 +1,4 @@
 
-/**
-*   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
-*   Print three lines:
-*   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
-*   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
-*   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
-*
-*	Parameter(s):
-*   secondInteger - The string representation of an integer.
-*   secondDecimal - The string representation of a floating-point number.
-*   secondString - A string consisting of one or more space-separated words.
-**/
 function performOperation(secondInteger, secondDecimal, secondString) {
     // Declare a variable named 'firstInteger' and initialize with integer value 4.
     const firstInteger = 4;
