58258c36ac1a69b4c408fc8c776191e3036ef201
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-07-01 10:52:06 +0530

    bfq

--- brain Freeze questions/Question2.java
+++ brain Freeze questions/Question2.java
@@ -1,6 +1,6 @@
 
 public class Question2 {
-    public static void main(String argv[]) {
+    public static void main(String args[]) {
         int arr[] = new int[10];
         int i = 5;
         arr[i++] = i++ + i++;
