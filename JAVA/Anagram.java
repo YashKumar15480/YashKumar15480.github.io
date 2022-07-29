8f94031a30be706af9449c0077960dad38f36980
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-07-29 11:41:08 +0530

    java

--- JAVA/Anagram.java
+++ JAVA/Anagram.java
@@ -11,7 +11,7 @@ class solutionn {
         System.out.print("enter string 1 =>");
         String a = nn.nextLine(); // taking input in string a
         System.out.print("enter string 2 =>");
-        String b = nn.nextLine(); // taking input in string b
+        String b = nn.nextLine();
 
         if (a.length() == b.length()) { // loop
             for (i = 0; i < a.length(); i++) {
