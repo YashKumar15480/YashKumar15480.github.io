60dc160d3bde5545700545511df3d974632f3430
YASH KUMAR <yash.2023mca1038@kiet.edu>
2022-09-18 00:44:21 +0530

    ASE

---
+++ Accenture Primers Learning ASE/first.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class first {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number = ");
+        int a = sc.nextInt();
+        System.out.println("you entered =>" + a);
+    }
+}
