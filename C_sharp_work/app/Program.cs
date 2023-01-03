532bf364531155427a93a5b916e259f9d5b4cb53
YASH KUMAR <72725598+YashKumar15480@users.noreply.github.com>
2023-01-03 14:38:30 +0530

    first_app

--- C_sharp_work/app/Program.cs
+++ C_sharp_work/app/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace app
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("First app try ==> ");
+
+            int a=10,b=20;
+
+            Console.WriteLine($"addition =>"+(a+b));
+            
+            
+        }
+    }
+}
