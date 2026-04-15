d8e77bbd635622efeffba2d4ddf1b684121208eb
YASH KUMAR <yaku15480@gmail.com>
2026-04-15 22:03:09 +0530

    committs

--- web-games/flappy-bird-ultra/js/upload.js
+++ web-games/flappy-bird-ultra/js/upload.js
@@ -27,7 +27,7 @@ imageUpload.addEventListener("change", function () {
     reader.readAsDataURL(file);
 });
 
-// 🖼 Draw preview canvass
+// 🖼 Draw preview can
 function drawPreview() {
     previewCtx.clearRect(0, 0, previewCanvas.width, previewCanvas.height);
 
