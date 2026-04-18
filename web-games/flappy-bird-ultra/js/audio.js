0cd63cac3f5aa45142861d36474f5b4a1c9a64a2
YASH KUMAR <yaku15480@gmail.com>
2026-04-18 21:08:17 +0530

    18th update

--- web-games/flappy-bird-ultra/js/audio.js
+++ web-games/flappy-bird-ultra/js/audio.js
@@ -11,7 +11,7 @@ let musicEnabled = true;
 
 bgAudio.volume = 0.05;
 
-// 🎵 BACKGROUND MUSIC TOGGLE
+// 🎵 BACKGROUND MUSIC TOGGLE added
 function toggleMusic() {
     musicEnabled = !musicEnabled;
 
